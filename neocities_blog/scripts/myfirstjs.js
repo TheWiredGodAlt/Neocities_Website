bb5242d6c1219007023d2db05f0eb7644220c364
WiredGodO_O <wiredgd66@gmail.com>
2026-07-24 10:43:17 -0300

    Just a test
    testing

--- neocities_blog/scripts/myfirstjs.js
+++ neocities_blog/scripts/myfirstjs.js
@@ -1,1 +1,21 @@
+//basic var declaration
+var a=1,str="basic"
+//Basic function, it takes the user to another window :)
+function traintest(){
+
+    testing=confirm("Are you sure???")
+    if(testing) window.location="https://google.com"
+};
+setTimeout.traintest, 60000
+//Function calling
+traintest()
+
+//Basic object training
+var obj={
+    test:20,
+    test2:["luke","Elize"]
+}
+//Basic print in JS
+console.log(a+1, obj.test,obj.test2[0])
 console.log("HelloWolrd!!")
+
