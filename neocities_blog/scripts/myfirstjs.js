8291281f03b580728cd4ae51c853db8622e65fc4
WiredGodO_O <wiredgd66@gmail.com>
2026-07-25 17:06:09 -0300

    File organization
    Just organizing some files of my blog

--- neocities_blog/scripts/myfirstjs.js
+++ neocities_blog/scripts/myfirstjs.js
@@ -1,17 +1,20 @@
 //basic var declaration
-var a=1,str="basic"
+var a=1;str="basic";
 //Basic function, it takes the user to another window :)
 function traintest(){
 
     testing=confirm("Are you sure???")
     if(testing) window.location="https://google.com"
 };
-setTimeout.traintest, 60000
-//Function calling
+setTimeout.traintest, 60000;
+//Function calling (i'm not calling it because i'm doing some other tests with this code (Owo))
 //Basic code that changes the html frontend (i still need to end this shit)
+//It also takes the user to the main google page using the window.location code
 document.getElementById('basicbtn').addEventListener('click', () => {
         document.getElementById('test23').textContent='I hate you all';
+        window.location="https://google.com"
     });
+
 
 
 
@@ -19,8 +22,8 @@ document.getElementById('basicbtn').addEventListener('click', () => {
 var obj={
     test:20,
     test2:["luke","Elize"]
-}
+};
 //Basic print in JS
-console.log(a+1, obj.test,obj.test2[0])
-console.log("HelloWolrd!!")
+console.log(a+1, obj.test,obj.test2[0]);
+console.log("HelloWolrd!!");
 
